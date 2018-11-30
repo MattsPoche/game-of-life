7d36f5975935c0c8e9434736ab25e585e69bd554
mattspoche <matthewspoche@comcast.net>
2018-11-29 21:32:43 -0600

    Added more patterns

--- src/main.rs
+++ src/main.rs
@@ -181,6 +181,8 @@ fn print_world(world: &Vec<Vec<bool>>) {
 fn main() {
     let mut world = init_world(80, 80);
     insert_pattern(&mut world, &get_pattern(Patterns::Toad), 10, 10);
+    insert_pattern(&mut world, &get_pattern(Patterns::Blinker), 0, 0);
+    insert_pattern(&mut world, &get_pattern(Patterns::Blinker), 76, 0);
     print_world(&world);
     for _i in 0..3 {
         let (lives, dies) = get_state(&world);
